6387a5f87a077682f477975cd9e17f5da9924c5c
pol-cova <83374367+pol-cova@users.noreply.github.com>
2022-10-13 12:14:03 -0500

    v0.0.5
    Add new packages for AI, and ml model

--- app/app.py
+++ app/app.py
@@ -166,4 +166,4 @@ def error_401():
     return render_template('error401.html', user_image = error401_img)
 
 if __name__ =='__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
